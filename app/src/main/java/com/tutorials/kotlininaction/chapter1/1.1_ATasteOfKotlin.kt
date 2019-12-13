97995e4f084a714beeae3c64d54ec1d7e58e57af
hannah <huilee9999@gmail.com>
2019-12-13 15:38:14 -0500

    chapter1

---
+++ app/src/main/java/com/tutorials/kotlininaction/chapter1/1.1_ATasteOfKotlin.kt
@@ -0,0 +1,24 @@
+package com.tutorials.kotlininaction.chapter1
+
+
+data class Person(val name : String, val age : Int?= null)
+
+
+fun main(args : Array<String>){
+
+    val persons = listOf(Person("Alice"), Person("Bob", age = 29))
+
+    // Elvis operator
+    val oldest = persons.maxBy { it.age ?: 0 }
+
+    println("The oldest is : $oldest" )
+
+}
+/**
+ * Functional Programming
+ *      Benifits : 1. Conciseness 2. safe multithreading  3. easy testing
+ *
+ */
+//fun findAlice() = findPerson { it.name == "Alice"}
+//fun findBob() = findPerson { it.name == "Bob" }
+
