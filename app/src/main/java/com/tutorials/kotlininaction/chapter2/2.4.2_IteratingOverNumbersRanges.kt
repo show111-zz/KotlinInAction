524cb99cde094bd79aaed660e7097153262bdf86
hannah <huilee9999@gmail.com>
2019-12-17 09:46:23 -0500

    chapter2.4.3

---
+++ app/src/main/java/com/tutorials/kotlininaction/chapter2/2.4.2_IteratingOverNumbersRanges.kt
@@ -0,0 +1,19 @@
+package com.tutorials.kotlininaction.chapter2
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun main(){
+//    for (i in 1..100){
+//        print(fizzBuzz(i))
+//    }
+
+    for (i in 100 downTo 1 step 2){
+        print(fizzBuzz(i))
+    }
+
+}
---
+++ app/src/main/java/com/tutorials/kotlininaction/chapter2/2.4.3_IteratingOverMaps.kt
@@ -0,0 +1,23 @@
+package com.tutorials.kotlininaction.chapter2
+
+import java.util.*
+
+val binaryReps = TreeMap<Char, String>()
+
+
+fun main(){
+    for(c in 'A'..'F'){
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryReps[c] = binary
+    }
+
+    for ((letter,binary) in binaryReps){
+        println("$letter = $binary")
+    }
+
+    val list = arrayListOf("10","11","1001")
+    for ((index, element) in list.withIndex()){
+        println("$index: $element")
+    }
+
+}
